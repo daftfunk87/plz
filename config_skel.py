64b2d01067343dd86805685bf8ef7ecb5cca08c7
daftfunk87 <jrstuart@live.com>
2016-12-08 15:16:57 -0700

    Add files via upload

---
+++ config_skel.py
@@ -0,0 +1,8 @@
+
+# This file contains the username and password to your bot.
+# DO NOT check it in!
+
+# Fill in the values and rename this file to config_bot.py
+
+REDDIT_USERNAME = 'picklebot9000'  # YOUR USERNAME as string
+REDDIT_PASS = 'Bouma9000'  # YOUR PASSWORD as string
